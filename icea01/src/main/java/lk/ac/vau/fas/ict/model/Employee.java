c62718464a5569e9fa038735cd38891369955f4b
Hiruni Ranasinghe <ranasinghehiruni14@gmail.com>
2025-05-09 22:19:54 +0530

    fixed errors

--- icea01/src/main/java/lk/ac/vau/fas/ict/model/Employee.java
+++ icea01/src/main/java/lk/ac/vau/fas/ict/model/Employee.java
@@ -3,7 +3,6 @@ package lk.ac.vau.fas.ict.model;
 import java.util.List;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
@@ -11,7 +10,6 @@ import jakarta.persistence.ManyToMany;
 @Entity
 public class Employee extends Person {
 
-	@Id
 	private String position;
 	@ManyToMany
 	@JoinTable(
