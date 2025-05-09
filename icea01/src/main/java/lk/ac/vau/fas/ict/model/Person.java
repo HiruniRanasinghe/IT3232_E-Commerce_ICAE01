f38dd265da6b526b0238f43cfe640b6d831c7d98
Hiruni Ranasinghe <ranasinghehiruni14@gmail.com>
2025-05-09 20:36:51 +0530

    create super calss

---
+++ icea01/src/main/java/lk/ac/vau/fas/ict/model/Person.java
@@ -0,0 +1,14 @@
+package lk.ac.vau.fas.ict.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Person {
+
+	@Id
+	private Long id;
+	private String name;
+	private int age;
+	private String gender;
+}
